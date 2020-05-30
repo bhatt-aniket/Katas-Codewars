fae7da1ef2cb754371eef07ad8776365f7a8e401
bhatt-aniket <66142695+bhatt-aniket@users.noreply.github.com>
2020-05-29 20:24:45 -0400

    Update main.py

--- 8-KYU/Remove First and Last Character Part Two/main.py
+++ 8-KYU/Remove First and Last Character Part Two/main.py
@@ -1,4 +1,4 @@
-#This is a spin off of my first kata. You are given a list of character sequences as a comma separated string. Write a function which returns another string containing all the character sequences except the first and the last ones, separated by spaces. If the input string is empty, or the removal of the first and last items would cause the string to be empty, return a null value.
+# Write a function which returns another string containing all the character sequences except the first and the last ones, separated by spaces. If the input string is empty, or the removal of the first and last items would cause the string to be empty, return a null value.
 
 def array(string):
   new_array = string.split(',')
@@ -12,4 +12,3 @@ def array(string):
       return x.replace(',', '')
   except IndexError:
     return None
-  
